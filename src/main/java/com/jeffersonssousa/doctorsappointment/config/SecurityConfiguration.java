ecd5a720047b83f971d32572acf84eccb1d15ad0
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-10-13 12:54:50 -0300

    feat: implements roles method for ADMIN and USER

--- src/main/java/com/jeffersonssousa/doctorsappointment/config/SecurityConfiguration.java
+++ src/main/java/com/jeffersonssousa/doctorsappointment/config/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package com.jeffersonssousa.doctorsappointment.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,11 +25,39 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(configurer ->{
-                    configurer.loginPage("/login").permitAll();
+                    configurer.loginPage("/login");
                 })
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/doctor/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/doctor/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/appointments/**").hasAnyRole("ADMIN","USER");
                     authorize.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        UserDetails user1 = User.builder()
+                .username("user")
+                .password(encoder.encode("1234567"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("1234567"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
     }
 }
