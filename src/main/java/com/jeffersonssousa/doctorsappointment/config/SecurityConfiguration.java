6ad5421e32ba8681a0fab6040fd84c2d46cd9dbe
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-10-30 12:38:20 -0300

    feat: A method for creating a user account when a Google user does not have access

--- src/main/java/com/jeffersonssousa/doctorsappointment/config/SecurityConfiguration.java
+++ src/main/java/com/jeffersonssousa/doctorsappointment/config/SecurityConfiguration.java
@@ -30,9 +30,9 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-                //.formLogin(configurer ->{
-                 //   configurer.loginPage("/login");
-                //})
+                .formLogin(configurer ->{
+                 configurer.loginPage("/login");
+                })
                 .formLogin(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/login").permitAll();
@@ -41,7 +41,9 @@ public class SecurityConfiguration {
                     authorize.anyRequest().authenticated();
                 })
                 .oauth2Login(oauth2 ->{
-                    oauth2.successHandler(successHandler);
+                    oauth2
+                            .loginPage("/login")
+                            .successHandler(successHandler);
                 })
                 .build();
     }
--- src/main/java/com/jeffersonssousa/doctorsappointment/security/LoginSocialSuccessHandler.java
+++ src/main/java/com/jeffersonssousa/doctorsappointment/security/LoginSocialSuccessHandler.java
@@ -9,16 +9,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private static final String SENHA_PADRAO = "1234567";
 
     private final UserService userService;
 
@@ -26,12 +28,16 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         super.onAuthenticationSuccess(request, response, authentication);
 
-        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken)  authentication;
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
         OAuth2User oAuth2User = oAuth2AuthenticationToken.getPrincipal();
 
         String email = oAuth2User.getAttribute("email");
 
         Login login = userService.getByEmail(email);
+
+        if (login == null) {
+            login = registerUser(email);
+        }
 
         authentication = new CustomAuthentication(login);
 
@@ -40,5 +46,16 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
         super.onAuthenticationSuccess(request, response, authentication);
     }
 
+
+    private Login registerUser(String email) {
+        Login login = new Login();
+        login.setEmail(email);
+        login.setLogin(email);
+        login.setPassword(SENHA_PADRAO);
+        login.setRoles(List.of("PACIENTE"));
+
+        userService.createUser(login);
+        return login;
+    }
 
 }
