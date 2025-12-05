8b3886ed7565bb3cda1fba8605595e328fba8de7
Jefferson sousa <jejefferson57@gmail.com>
2025-12-05 10:01:04 -0300

    feat: create CustomRegisteredClientRepository and AuthorizationServerConfiguration

---
+++ src/main/java/com/jeffersonssousa/doctorsappointment/security/CustomRegisteredClientRepository.java
@@ -0,0 +1,55 @@
+package com.jeffersonssousa.doctorsappointment.security;
+
+import com.jeffersonssousa.doctorsappointment.entity.Client;
+import com.jeffersonssousa.doctorsappointment.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    @Autowired
+    private ClientService service;
+
+    @Autowired
+    private TokenSettings tokenSettings;
+
+    @Autowired
+    private ClientSettings clientSettings;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        Client client = service.findByClientId(clientId);
+
+        if (client == null){
+            return  null;
+        }
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectURI())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
+                .build();
+    }
+}
