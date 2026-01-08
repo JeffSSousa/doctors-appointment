f7ed008275a4ac3e76cd65bc9d3d849e178b4b6b
Jefferson sousa <jejefferson57@gmail.com>
2026-01-08 16:30:14 -0300

    refactor: delete registerDoctor and delete patientController

--- src/main/java/com/jeffersonssousa/doctorsappointment/controller/DoctorController.java
+++ src/main/java/com/jeffersonssousa/doctorsappointment/controller/DoctorController.java
@@ -2,17 +2,11 @@ package com.jeffersonssousa.doctorsappointment.controller;
 
 import com.jeffersonssousa.doctorsappointment.controller.mappers.DoctorMapper;
 import com.jeffersonssousa.doctorsappointment.dto.doctor.DoctorResponseDTO;
-import com.jeffersonssousa.doctorsappointment.entity.Doctor;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import com.jeffersonssousa.doctorsappointment.dto.doctor.DoctorRequestDTO;
 import com.jeffersonssousa.doctorsappointment.service.DoctorService;
-
 import java.util.List;
 
 @RestController
@@ -24,14 +18,6 @@ public class DoctorController {
 
     @Autowired
     private DoctorMapper doctorMapper;
-
-	@PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Void> registerDoctor(@RequestBody @Valid DoctorRequestDTO dto){
-        Doctor doctor = doctorMapper.toEntity(dto);
-        service.insert(doctor);
-		return ResponseEntity.status(HttpStatus.CREATED).build();
-	}
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
