26333752e99cc7e2754794004ebf4d62f0fe693d
Jefferson Sousa <jejefferson57@gmail.com>
2026-01-16 15:31:58 -0300

    test: recreate insert tests in DoctorServiceTest and add exception scenarios

--- src/test/java/com/jeffersonssousa/doctorsappointment/service/DoctorServiceTest.java
+++ src/test/java/com/jeffersonssousa/doctorsappointment/service/DoctorServiceTest.java
@@ -1,9 +1,15 @@
 package com.jeffersonssousa.doctorsappointment.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
+import com.jeffersonssousa.doctorsappointment.builders.DoctorTestBuilder;
+import com.jeffersonssousa.doctorsappointment.builders.PatientTestBuilder;
+import com.jeffersonssousa.doctorsappointment.entity.Login;
+import com.jeffersonssousa.doctorsappointment.entity.Patient;
+import com.jeffersonssousa.doctorsappointment.exception.business.DoctorAlreadyLinkedException;
+import com.jeffersonssousa.doctorsappointment.exception.business.PatientAlreadyLinkedException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +22,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.jeffersonssousa.doctorsappointment.entity.Doctor;
 import com.jeffersonssousa.doctorsappointment.repository.DoctorRepository;
 
+import java.util.UUID;
+
 @ExtendWith(MockitoExtension.class)
 public class DoctorServiceTest {
 	
@@ -27,26 +35,65 @@ public class DoctorServiceTest {
 	
 	@Captor
 	private ArgumentCaptor<Doctor> doctorCaptor;
-	
-//	@Test
-//	@DisplayName("Deve inserir um médico corretamente.")
-//	void shouldCreateADoctor() {
-//
-//		//Arrange
-//		Doctor entity = new Doctor(null,"Fulano", "fulano@email.com", "99999-9999", "5265-5", "Clinico Geral",null);
-//
-//		//Act
-//		service.insert(entity);
-//
-//		//Assert & Verify
-//		verify(repository, times(1)).save(doctorCaptor.capture());
-//		Doctor doctor = doctorCaptor.getValue();
-//
-//		assertEquals(entity.getName(), doctor.getName());
-//		assertEquals(entity.getEmail(), doctor.getEmail());
-//		assertEquals(entity.getPhone(), doctor.getPhone());
-//		assertEquals(entity.getCrm(), doctor.getCrm());
-//		assertEquals(entity.getSpecialty(), doctor.getSpecialty());
-//
-//	}
+
+
+
+
+	@Test
+	@DisplayName("Deve inserir um médico corretamente.")
+	void shouldCreateADoctor() {
+
+		//Arrange
+        Login login = new Login();
+        UUID userId = UUID.randomUUID();
+        login.setUserId(userId);
+
+        Doctor doctor = DoctorTestBuilder
+                            .aDoctor()
+                            .withLogin(login)
+                            .build();
+
+        when(repository.save(any(Doctor.class))).thenReturn(doctor);
+        when(repository.existsByLogin_UserId(any(UUID.class))).thenReturn(false);
+
+		//Act
+		service.insert(doctor);
+
+		//Assert & Verify
+        verify(repository,times(1)).existsByLogin_UserId(any(UUID.class));
+		verify(repository, times(1)).save(doctorCaptor.capture());
+
+		Doctor returnedDoctor = doctorCaptor.getValue();
+
+		assertEquals(doctor.getName(), returnedDoctor.getName());
+		assertEquals(doctor.getEmail(), returnedDoctor.getEmail());
+		assertEquals(doctor.getPhone(), returnedDoctor.getPhone());
+		assertEquals(doctor.getCrm(), returnedDoctor.getCrm());
+		assertEquals(doctor.getSpecialty(), returnedDoctor.getSpecialty());
+
+	}
+
+    @Test
+    @DisplayName("Deve lançar exceção quando o usuario já estiver vinculado com um médico")
+    void shouldThrowExceptionWhenUserAlreadyLinked(){
+
+        Login login = new Login();
+        UUID userId = UUID.randomUUID();
+        login.setUserId(userId);
+
+        Doctor doctor = DoctorTestBuilder
+                .aDoctor()
+                .withLogin(login)
+                .build();
+
+        when(repository.existsByLogin_UserId(any(UUID.class))).thenReturn(true);
+
+        DoctorAlreadyLinkedException e = assertThrows(DoctorAlreadyLinkedException.class,
+                () -> service.insert(doctor));
+
+        verify(repository, never()).save(any());
+
+        assertEquals("Já existe um doutor vinculado com esse acesso", e.getMessage());
+
+    }
 }
