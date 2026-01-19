d45917824b5d67ed11836d01bfcdfaf6930b2699
Jefferson Sousa <jejefferson57@gmail.com>
2026-01-19 13:01:11 -0300

    refactor: delete boirplates in AppointmentResponseDTO and DoctorResponseDTO

--- src/main/java/com/jeffersonssousa/doctorsappointment/dto/appointment/AppointmentResponseDTO.java
+++ src/main/java/com/jeffersonssousa/doctorsappointment/dto/appointment/AppointmentResponseDTO.java
@@ -5,9 +5,5 @@ import java.time.LocalDateTime;
 import com.jeffersonssousa.doctorsappointment.entity.Appointment;
 
 public record AppointmentResponseDTO (String patient, boolean isReturn , LocalDateTime appointmentDate, Integer durationInMinutes) {
-	
-	public AppointmentResponseDTO(Appointment appointment) {
-		this(appointment.getPatient().getName(), appointment.isReturn(), appointment.getAppointmentDate(), appointment.getDurationInMinutes());
-	}
-	
+
 }
--- src/main/java/com/jeffersonssousa/doctorsappointment/dto/doctor/DoctorResponseDTO.java
+++ src/main/java/com/jeffersonssousa/doctorsappointment/dto/doctor/DoctorResponseDTO.java
@@ -4,7 +4,4 @@ import com.jeffersonssousa.doctorsappointment.entity.Doctor;
 
 public record DoctorResponseDTO(Long doctorId, String name, String email, String phone, String crm, String specialty) {
 
-    public DoctorResponseDTO (Doctor doctor){
-        this(doctor.getDoctorId(), doctor.getName(), doctor.getEmail(), doctor.getPhone(), doctor.getCrm(), doctor.getSpecialty());
-    }
 }
