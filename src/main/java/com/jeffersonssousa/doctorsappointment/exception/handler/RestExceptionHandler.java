ceb31a7d3a477628b6ce83c299ae45e25ab76c9c
Jefferson Sousa <jejefferson57@gmail.com>
2026-01-19 13:18:13 -0300

    feat: add Exception handler for MethodArgumentNotValidException class

--- src/main/java/com/jeffersonssousa/doctorsappointment/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/jeffersonssousa/doctorsappointment/exception/handler/RestExceptionHandler.java
@@ -4,10 +4,13 @@ import com.jeffersonssousa.doctorsappointment.exception.business.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -116,6 +119,26 @@ public class RestExceptionHandler {
                 status.value(),
                 error,
                 e.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> MethodArgumentNotValid (MethodArgumentNotValidException e, HttpServletRequest request){
+
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+
+
+        String error = "Erro de Validação";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(),
+                status.value(),
+                error,
+                message,
                 request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
